1fbe90f057f5d3a380d2a3782b3c07b513533048
arsanuos <arsanuos.attia@gmail.com>
2018-10-24 19:37:59 +0200

    fix hassing issue: hash array instead of cost+ heureist

--- classes/AStar.py
+++ classes/AStar.py
@@ -17,4 +17,8 @@ class AStar(Agent):
         while len(states_heap):
             # break tie by FIFO criteria
             current_explored_state = nsmallest(states_heap, key=lambda x, y: x.cost < y.cost)
-            self.__vis.add(current_explored_state)
+            if current_explored_state not in self.__vis:
+                self.__vis.add(current_explored_state)
+                child_states = self.expand()
+                for child in child_states:
+                    if child not in self.__vis:
