a9858cb4e978494007f883b9ac5fecec5d0ce89a
Amr Hendy <amr.m.hendy@gmail.com>
2018-10-25 00:01:27 +0200

    Merging with Essam

---
+++ classes/dfs.py
@@ -0,0 +1,26 @@
+from agent import Agent
+from state import State
+
+
+class DFS(Agent):
+
+    def search(self, initial_state):
+
+        start = State(initial_state, None)
+        frontier = [start]
+        final_state = None
+        while frontier:
+            state = frontier.pop()
+            self.vis.add(state)
+
+            if state.is_goal():
+                final_state = state
+                break
+
+            neighbours = self.expand(state)
+            for neighbour in neighbours:
+                if neighbour not in frontier:
+                    frontier.append(neighbour)
+
+        steps = self.get_steps(final_state)
+        return steps
