1361d466103fd0da004437c7477b125615b4f6ef
Amr Hendy <amr.m.hendy@gmail.com>
2018-10-24 19:03:26 +0200

    Fixing Agent Class and Added BFS Skelton

--- classes/agent.py
+++ classes/agent.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
-from copy import copy
+from copy import deepcopy
 from state import State
+
 
 class Agent(ABC):
 
     def __init__(self):
+        # vis is set of arrays not states
         self.__vis = set()
 
     def expand(self, current_state, heuristic=None):
@@ -17,10 +19,20 @@ class Agent(ABC):
         dy = [0, 0, 1, -1]
         for x, y in zip(dx, dy):
             if 0 <= (empty_index[0] + x) <= 2 and 0 <= (empty_index[1] + y) <= 2:
-                new_arr = copy(current_state.current)
+                new_arr = deepcopy(current_state.current)
                 old_index = new_arr.index(0)
                 new_index = (empty_index[0] + x) * 3 + (empty_index[1] + y)
                 new_arr[old_index], new_arr[new_index] = new_arr[new_index], new_arr[old_index]
-                child_states.append(State(new_arr, current_state, heuristic))
+                if new_arr not in self.vis:
+                    child_states.append(State(new_arr, current_state, heuristic))
 
         return child_states
+
+    @property
+    def vis(self):
+        return self.__vis
+
+    @abstractmethod
+    def search(self, initial_state):
+        pass
+
---
+++ classes/bfs.py
@@ -0,0 +1,8 @@
+from agent import Agent
+from state import State
+
+class BFS(Agent):
+
+    def search(self, initial_state):
+        steps = []
+        return steps
