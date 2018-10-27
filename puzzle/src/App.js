15105c314e14f62960737678579666a7dbb6496d
arsanuos <arsanuos.attia@gmail.com>
2018-10-27 19:29:50 +0200

    add padding to container for a better ui

--- puzzle/src/App.js
+++ puzzle/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Board from './Board.js';
 import Details from './Details.js';
+import './App.css';
 const axios = require('axios');
 
 export default class App extends Component{
